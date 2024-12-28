314a39f83fd910e6f1b7d4898f9031c1c45ac707
Norawee Laohateerapong <StarlinzSama@gmail.com>
2024-12-28 19:59:34 +0700

    hw1 done.

--- ep1/hw.py
+++ ep1/hw.py
@@ -1,15 +1,30 @@
 # 28 Dec 2024 - Nine
+# Homework instruction: draw 10 square in a random position
 import turtle as tao, random
 
-def create_circle(random_radius=False ,radius=100, count=10, speed=5):
-    angle = 360 / count
-    colors = ['red', 'green', 'blue']
-    tao.speed(speed)
-    tao.pensize(3)
-    if random_radius:
-        print('hello')
+class Shape:
+    def __init__(self):
+        pass
+
+    def random_pos(self, rand_range=(-200, 200)):
+        x_pos = random.randint(*rand_range)
+        y_pos = random.randint(*rand_range)
+        pos = (x_pos, y_pos)
+        return pos
+
+    def draw_circles(self, random_radius=False ,radius=100, random_pos=False, count=10, speed=5):
+        angle = 360 / count
+        colors = ['red', 'green', 'blue']
+        tao.speed(speed)
+        tao.pensize(3)
         for i in range(count):
-            radius = random.randint(10, 100)
+            if random_radius:
+                radius = random.randint(10, 100)
+            if random_pos:
+                position = self.random_pos()
+                tao.penup()
+                tao.goto(*position)
+                tao.pendown()
             color = random.choice(colors)
             tao.color(color)
             tao.circle(radius=radius)
@@ -17,15 +32,6 @@ def create_circle(random_radius=False ,radius=100, count=10, speed=5):
             print(f"หมุนครั้งที่: {i + 1}")
         print("END")
         tao.Screen().exitonclick()
-    else:
-        print('yolle')
-        for i in range(count):
-            color = random.choice(colors)
-            tao.color(color)
-            tao.circle(radius=radius)
-            tao.left(angle)
-            print(f"หมุนครั้งที่: {i + 1}")
-        print("END")
-        tao.Screen().exitonclick()
 
-create_circle(random_radius=True, radius=25, count=3) 
+c1 = Shape()
+c1.draw_circles(random_radius=False, random_pos=True)
