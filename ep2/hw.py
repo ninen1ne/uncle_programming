813d65c28d07bf5740adab5f6dfe655d45ce22de
Norawee Laohateerapong <StarlinzSama@gmail.com>
2024-12-31 17:49:21 +0700

    final update for hw2.

--- ep2/hw.py
+++ ep2/hw.py
@@ -4,7 +4,14 @@ import turtle as tao, random
 
 class Shape:
     def __init__(self):
-        self.colors = ['red', 'green', 'blue', 'pink', 'violet', 'black']
+        self.color_range = "0123456789abcdef"
+        self.colors = ['red', 'green', 'blue', 'yellow', 'pink', 'violet', 'black']
+
+    def create_hex_color(self):
+        hex_color = "#"
+        for i in range(6):
+            hex_color += self.color_range[random.randint(0, 15)]
+        return hex_color
 
     def random_pos(self, rand_range=(-200, 200)):
         x_pos = random.randint(*rand_range)
@@ -25,11 +32,12 @@ class Shape:
                 tao.penup()
                 tao.goto(*position)
                 tao.pendown()
-            color = random.choice(self.colors)
+            # color = random.choice(self.colors)
+            color = self.create_hex_color()
             tao.color(color)
             tao.circle(radius=radius)
             tao.left(angle) # หันหน้าเอียงทีละกี่องศา
-            print(f"หมุนครั้งที่: {i + 1}")
+            print(f"วาดครั้งที่: {i + 1} || color: {color}")
         print("END")
         # tao.Screen().exitonclick()
 
@@ -45,12 +53,13 @@ class Shape:
                 tao.pendown()
             if random_length:
                 length = random.randint(25, 150)
-            color = random.choice(self.colors)
+            # color = random.choice(self.colors)
+            color = self.create_hex_color()
             tao.color(color)
             for j in range(4):
                 tao.forward(length)
                 tao.left(90)
-            print(f"วาดครั้งที่ {i + 1}")
+            print(f"วาดครั้งที่ {i + 1} || color: {color}")
         print("END")
         # tao.Screen().exitonclick()
 
---
+++ test.py
@@ -0,0 +1,6 @@
+import random
+
+alphabets = 'abcdefg'
+
+for i in range(10):
+    print(alphabets[random.randint(0, 6)])
