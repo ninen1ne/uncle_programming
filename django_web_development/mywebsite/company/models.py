8446be45ac55f47a11183fd8d64c6244bcd27d6c
Norawee Laohateerapong <StarlinzSama@gmail.com>
2025-01-13 22:18:19 +0700

    create first model(database in Django).
    This model is called product, product contain general information about books in the store.

--- django_web_development/mywebsite/company/models.py
+++ django_web_development/mywebsite/company/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.(database)
+
+"""
+Product(name of table)
+    - title (Char)
+    - description (Text)
+    - price (Decimal)
+    - quantity (Int)
+"""
+
+"""
+After create or change models run 2 commands from terminal:
+1) python manage.py makemigrations
+2) python manage.py migrate
+"""
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    quantity = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- django_web_development/mywebsite/company/views.py
+++ django_web_development/mywebsite/company/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 # def home(request):
 #     return HttpResponse('<h1>Hello World!</h1> <br> <p>By Norawee Yolle</p>')
 
 def home(request):
-    return render(request, 'company/home.html')
+    all_product = Product.objects.all()
+    context = {'all_product': all_product} # SELECT * from product
+    return render(request, 'company/home.html', context)
     #'DIRS': [BASE_DIR / 'company/template'], + 'company/home.html' (base_dir is location where a project are stored)
 
 def about_us(request):
